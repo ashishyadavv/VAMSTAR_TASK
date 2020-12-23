52f4aedd71cdb2f7b78fafa79a562b86df4fec2b
Ashish yadav <ashish.yadav2021@gmail.com>
2020-12-23 22:31:34 +0530

    CssSelectors

---
+++ script.js
@@ -0,0 +1,14 @@
+const publicationData=document.querySelector('.lbl-licitacao')
+document.onreadystatechange = () => {
+    const object=document.querySelector('.lbl-licitacao')
+    const bidding_data=document.querySelector('.lbl-licitacao')
+    const links=document.querySelectorAll("a")
+
+    console.log(`PublicationData : ${publicationData}`)
+    console.log(`Object : ${object}`)
+    console.log(`Bidding_data : ${publicationData}`)
+    console.log(`All links : ${links}`)
+  };
+
+
+
